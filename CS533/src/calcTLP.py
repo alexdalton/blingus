2515641aef878cec9b317ee022c1cd6bd93b2b30
Alex Dalton <alexmdalton@gmail.com>
2016-04-30 18:55:26 -0500

    Fixed TLP calculation added comments

--- CS533/src/calcTLP.py
+++ CS533/src/calcTLP.py
@@ -1,31 +1,37 @@
 from optparse import OptionParser
 import matplotlib.pyplot as plt
 import os
+from tabulate import tabulate
 
 parser = OptionParser()
-parser.add_option("-f", "--file", dest="filename", help="ftrace output text file", metavar="FILE")
+parser.add_option("-f", "--file", dest="filename", help="ftrace output text file name", metavar="FILE")
 
 (options, args) = parser.parse_args()
 
+# directory with all the trace files
 traces_dir = "/home/alex/blingus/CS533/traces/"
 
+# If no file option included do all the files in the trace directory
 if not options.filename:
     files = os.listdir(traces_dir)
+# Otherwise run the TLP analysis for just the given trace file
 else:
     files = [options.filename]
 
+tlps = []
 for file in files:
     trace_fd = open(os.path.join(traces_dir, file), "r")
 
-    cpuState = [0] * 8
-    times = []
-    activeCPUs = []
+    cpuState = [0] * 8      # initialize all cpus to off
+    times = []              # array to hold timestamps
+    activeCPUs = []         # array to hold number of active cpus at each timestamp
 
     for line in trace_fd:
-        # ignore comments
+        # ignore lines that begin with '#' (commented lines)
         if line.lstrip()[0] == "#":
             continue
 
+        # split line by whitespace
         items = line.split()
 
         try:
@@ -34,10 +40,13 @@ for file in files:
             cpu = None
             time = None
             function = None
+
+            # Case for first type of trace file
             if items[1][0] == "[":
                 cpu = int(items[1].strip("[]"))     # get CPU number from line
                 time = float(items[2].rstrip(":"))  # get timestamp from line
                 function = items[3].rstrip(":")
+            # Case for second type of trace file
             elif items[2][0] == "[":
                 cpu = int(items[2].strip("[]"))     # get CPU number from line
                 time = float(items[4].rstrip(":"))  # get timestamp from line
@@ -47,29 +56,47 @@ for file in files:
         except IndexError:
             continue
 
+        # Catch for incorrectly formatted line
         if cpu is None or time is None or function is None:
             continue
 
+        # If the task is adbd set CPU to idle (ignore it)
         if task == "adbd":
             cpuState[cpu] = 0
+        # If we have a sched_switch task and going to the swapper set to idle
         elif function == "sched_switch":
             if line.find("==> swapper") != -1:
                 cpuState[cpu] = 0
             if line.find("==> next_comm=swapper"):
                 cpuState[cpu] = 0
+        # Otherwise CPU is active at this time
         else:
             cpuState[cpu] = 1
 
         times.append(time)
         activeCPUs.append(cpuState.count(1))
 
-    top = 0
-    bottom = 0
-    for i in range(0, len(times)):
-        top += activeCPUs[i] * times[i]
-        if activeCPUs[i] != 0:
-            bottom += times[i]
+    trace_fd.close()
 
-    print file, float(top) / float(bottom)
+    tlpNumerator = 0
+    tlpDenominator = 0
+    offset = times[0]       # First time stamp is amount to offset each time by
+    times[0] = 0.0          # Make sure we set first timestamp to 0
 
-    plt.plot(times, activeCPUs, ls='steps')
+    for i in range(1, len(times)):
+        times[i] -= offset      # Offset by first timestamp
+        if activeCPUs[i] != 0:  # Don't care about 0 active CPUs
+            tlpNumerator += (activeCPUs[i] * (times[i] - times[i-1]))
+            tlpDenominator += (times[i] - times[i-1])
+
+
+    tlps.append([file, float(tlpNumerator) / float(tlpDenominator)])
+
+    # Uncomment to plot Active CPUs vs Time
+    # plt.plot(times, activeCPUs, ls='steps')
+    # plt.ylabel("# of Active CPUs")
+    # plt.xlabel("Time (s)")
+    # plt.show()
+
+# Print out TLPs in a nice format
+print tabulate(tlps, headers=["Trace File", "TLP"])
