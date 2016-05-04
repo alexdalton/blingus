0a7bbc518edb1562bf1f8b92c41599b2d660e383
Alex Dalton <alexmdalton@gmail.com>
2016-05-03 23:48:32 -0500

    stuff

--- CS533/src/calcTLP.py
+++ CS533/src/calcTLP.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 import os
 from tabulate import tabulate
 
+# Configuration variables
+traces_dir = "/home/alex/blingus/CS533/traces/"  # directory with all the trace files
+numCores = 8                                     # number of cores for this device
+
+
 parser = OptionParser()
 parser.add_option("-f", "--file", dest="filename", help="ftrace output text file name", metavar="FILE")
 
 (options, args) = parser.parse_args()
-
-# directory with all the trace files
-traces_dir = "/home/alex/blingus/CS533/traces/"
 
 # If no file option included do all the files in the trace directory
 if not options.filename:
@@ -20,114 +22,136 @@ else:
 
 tlps = []
 for file in files:
+    print("Running on trace file: {0}".format(file))
     trace_fd = open(os.path.join(traces_dir, file), "r")
 
-    cpuState = [0] * 8      # initialize all cpus to off
-    times = []              # array to hold timestamps
-    activeCPUs = []         # array to hold number of active cpus at each timestamp
-    cpuFreq = [[] for k in xrange(8)]
-    cpuFreqTimes = [[] for j in xrange(8)]
+    cpuState = [0] * numCores      # initialize all cpus to off
+    eventTimes = []                     # array to hold timestamps
+    activeCPUs = []                # array to hold number of active cpus at each timestamp
 
+    cpuFreqs = []                  # Holds numCore arrays to record frequency of given core
+    cpuFreqTimes = []              # Holds numCore arrays to record timestamps the frequency is taken at
+    cpuTasks = []
+    cpuTaskTimes = []
+    taskAggTimes = {}
+    for i in xrange(numCores):
+        cpuFreqs.append([])
+        cpuFreqTimes.append([])
+        cpuTasks.append([])
+        cpuTaskTimes.append([])
+
+
+    offset = None                  # The time offset (time of the first timestamp)
+    cpufunctions = set()
+    counter = 0
     for line in trace_fd:
         # ignore lines that begin with '#' (commented lines)
         if line.lstrip()[0] == "#":
             continue
 
-        # split line by whitespace
-        items = line.split()
+        items = line.split(":")
+        if len(items) < 3:
+            continue
+
+        task_pid = line[0]
+        for i in range(1, len(line)):
+            if line[i - 1] == " " and (line[i] == "[" or line[i] == "("):
+                break
+            task_pid += line[i]
+
+        pid = task_pid.split("-")[-1]
+        task = task_pid[0:-len(pid) - 1]
+        cpuNum = int(line[len(pid) + len(task):].split("[")[1][0:3])
+        pid = int(pid)
 
         try:
-            pid = items[0].split("-")[-1]
-            task = items[0][0:-1 - len(pid)]
-            cpu = None
-            time = None
-            function = None
-
-            # Case for first type of trace file
-            if items[1][0] == "[":
-                cpu = int(items[1].strip("[]"))     # get CPU number from line
-                time = float(items[2].rstrip(":"))  # get timestamp from line
-                function = items[3].rstrip(":")
-            # Case for second type of trace file
-            elif items[2][0] == "[":
-                cpu = int(items[2].strip("[]"))     # get CPU number from line
-                time = float(items[4].rstrip(":"))  # get timestamp from line
-                function = items[5].rstrip(":")
+            time = float(items[0].split()[-1])
+            function = items[1].strip()
         except ValueError:
-            continue
-        except IndexError:
-            continue
+            time = float(items[1].split()[-1])
+            function = items[2].strip()
 
-        # Catch for incorrectly formatted line
-        if cpu is None or time is None or function is None:
-            continue
+        if offset is None:
+            offset = time
+
+        time -= offset
 
         # If the task is adbd set CPU to idle (ignore it)
-        if task == "adbd":
-            cpuState[cpu] = 0
-            # cpuFreqTimes[cpu].append(time)
-            # cpuFreq[cpu].append(0)
-        # If we have a sched_switch task and going to the swapper set to idle
-        elif function == "sched_switch":
-            if line.find("==> swapper") != -1:
-                cpuState[cpu] = 0
-                cpuFreqTimes[cpu].append(time)
-                cpuFreq[cpu].append(0)
-            if line.find("==> next_comm=swapper"):
-                cpuState[cpu] = 0
-                cpuFreqTimes[cpu].append(time)
-                cpuFreq[cpu].append(0)
-        elif function[0:7] == "cpufreq":
-            cpuFreqInfo = line.split(':')[2]
-            for info in cpuFreqInfo.split():
-                if info[0:4] == "cur=":
-                    cpuFreq[cpu].append(int(info[4:]))
-                    cpuFreqTimes[cpu].append(time)
-        # Otherwise CPU is active at this time
-        else:
-            cpuState[cpu] = 1
+        if (task == "adbd") or (task == "<idle>"):
+            cpuState[cpuNum] = 0
 
-        times.append(time)
+        if function == "sched_switch":
+            if (line.find("==> swapper") > -1) or (line.find("==> next_comm=swapper") > -1):
+                cpuState[cpuNum] = 0
+            else:
+                cpuState[cpuNum] = 1
+        elif function.find("cpufreq") > -1:
+            for item in line.split():
+                if item[0:4] == "cur=":
+                    cpuFreqs[cpuNum].append(int(item.split("=")[1]))
+                    cpuFreqTimes[cpuNum].append(time)
+
+        cpuTasks[cpuNum].append(pid)
+        cpuTaskTimes[cpuNum].append(time)
+        eventTimes.append(time)
         activeCPUs.append(cpuState.count(1))
 
     trace_fd.close()
 
-    offset = times[0]       # First time stamp is amount to offset each time by
-    for cpuFreqTime in cpuFreqTimes:
-        for i in range(0, len(cpuFreqTime)):
-            cpuFreqTime[i] -= offset
+    for i in xrange(8):
+        trackTask = None
+        trackTaskStart = None
+        for j in xrange(len(cpuTasks[i])):
+            currentTask = cpuTasks[i][j]
+            currentTime = cpuTaskTimes[i][j]
+            if trackTask is None or trackTaskStart is None:
+                trackTask = currentTask
+                trackTaskStart = currentTime
+            elif trackTask == currentTask:
+                continue
+            else:
+                if trackTask not in taskAggTimes:
+                    taskAggTimes[trackTask] = 0
+                taskAggTimes[trackTask] += currentTime - trackTaskStart
+                trackTask = currentTask
+                trackTaskStart = currentTime
 
     tlpNumerator = 0
     tlpDenominator = 0
-    times[0] = 0.0          # Make sure we set first timestamp to 0
-
     tlpTimes = [0.0]
-    for i in range(1, len(times)):
-        times[i] -= offset      # Offset by first timestamp
+    for i in range(1, len(eventTimes)):
         if activeCPUs[i] != 0:  # Don't care about 0 active CPUs
-            tlpNumerator += (activeCPUs[i] * (times[i] - times[i-1]))
-            tlpDenominator += (times[i] - times[i-1])
-        try:
+            tlpNumerator += (activeCPUs[i] * (eventTimes[i] - eventTimes[i-1]))
+            tlpDenominator += (eventTimes[i] - eventTimes[i-1])
             tlpTimes.append(float(tlpNumerator) / float(tlpDenominator))
-        except ZeroDivisionError:
-            tlpTimes.append(0.0)
+        else:
+            tlpTimes.append(tlpTimes[-1])
 
     tlps.append([file, float(tlpNumerator) / float(tlpDenominator)])
 
     plt.figure(1)
+    plt.plot(eventTimes, tlpTimes)
+
+    plt.figure(2)
     for i in range(0, 8):
         plt.subplot(241 + i)
         if len(cpuFreqTimes[i]) > 0:
-            plt.axis([0, times[-1], 0, max(cpuFreq[i])])
-            plt.plot(cpuFreqTimes[i], cpuFreq[i], ls='steps')
+            cpuFreqTimes[i] = [0] + cpuFreqTimes[i] + [eventTimes[-1]]
+            cpuFreqs[i] = [cpuFreqs[i][0]] + cpuFreqs[i] + [cpuFreqs[i][-1]]
+            plt.axis([0, eventTimes[-1], 0, max(cpuFreqs[i])])
+            plt.plot(cpuFreqTimes[i], cpuFreqs[i], ls='steps')
 
+    plt.figure(3)
+    plt.plot(eventTimes, activeCPUs, ls='steps')
+
+    x = []
+    labels = []
+    for k, v in taskAggTimes.iteritems():
+        x.append(v)
+        labels.append(k)
+
+    plt.figure(4)
+    plt.pie(x, labels=labels)
     plt.show()
-
-    # Uncomment to plot Active CPUs vs Time
-    # plt.plot(times, tlpTimes)
-    # plt.ylabel("# of Active CPUs")
-    # plt.xlabel("Time (s)")
-    # plt.show()
-
 # Print out TLPs in a nice format
 print tabulate(tlps, headers=["Trace File", "TLP"])
